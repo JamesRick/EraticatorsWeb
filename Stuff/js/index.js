cc3945c11ff7be66bb03263bd93c921abd9b87e7
JamesRick <james.rick.1995@gmail.com>
2017-11-27 20:22:11 -0500

    Working on database. Needs more work.

--- Stuff/js/index.js
+++ Stuff/js/index.js
@@ -2,6 +2,7 @@
 
 app.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
 
+var help = app.auth();
 
 function handleChange(checkBox) {
     if (checkBox.checked == true) {
@@ -13,6 +14,27 @@ function handleChange(checkBox) {
 // auth = firebase.auth().setPersistence(firebase.auth().Auth.Persistence.SESSION).catch(function (error) {
 //     console.log(error.message);
 // });
+
+function googleLogin() {
+    var provider = new firebase.auth.google;
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
 
 function Login() {
     var txtEmail = document.getElementById("user").value;
