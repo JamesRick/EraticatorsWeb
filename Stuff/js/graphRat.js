3cfaf65df5d99be50dd8e7ba48e404ebb1b871e0
christinefeng <chfeng15@gmail.com>
2017-11-28 19:56:59 -0500

    added more to graph

--- Stuff/js/graphRat.js
+++ Stuff/js/graphRat.js
@@ -1,8 +1,10 @@
 function GraphRats() {
     var startdate = document.getElementById("startdate").value;
     var enddate = document.getElementById("enddate").value;
-    const data = firebase.database();
-    const dayRef = data.ref("DAYS_TABLE");
+    // var startdate = sd.substring(5,8) + sd.substring(8) + "-" + sd.substring(0,5);
+    // var enddate = ed.substring(5,8) + ed.substring(8) + "-" + ed.substring(0,5);
+    const db = firebase.database();
+    const dayRef = db.ref("WEB_DAYS_TABLE");
 
     var svg = d3.select("svg"),
         margin = {top: 20, right: 20, bottom: 30, left: 40},
@@ -15,14 +17,14 @@ function GraphRats() {
     var g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var dayMap = new Map();
+    var data = d3.map();
 
     dayRef.orderByKey().startAt(startdate).endAt(enddate)
         .once("value", function(snapshot){
-            alert(snapshot);
             snapshot.forEach(function(children) {
-                alert(children.key);
-                console.log(children.key);
+                console.log(children.numChildren());
+                //console.log("date: %s; num: %d", children.key, children.val().size);
+                data.set(children.key, children.numChildren());
             });
         });
 
@@ -59,5 +61,33 @@ function GraphRats() {
     //         .attr("width", x.bandwidth())
     //         .attr("height", function(d) { return height - y(d.frequency); });
     // });
+
+    x.domain(d3.extent(data, function(d) { return d; }));
+    y.domain([0, d3.max(data, function(d) {
+        return data.get(d); })]);
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10, "%"))
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Frequency");
+
+    g.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d); })
+        .attr("y", function(d) { return y(data.get(d)); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(data.get(d)); });
 
 }
